9a79a57a5f8d309f16f4beaf267ad231b533d2ea
Max Shaffer <max@Maxs-MBP.attlocal.net>
2021-06-05 17:43:39 -0400

    fixed for real

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -6,7 +6,7 @@ switch (window.location.hostname) {
     case '127.0.0.1' :
         APIURL = 'http://localhost:8080'
         break
-    case 'https://throwback-theater.herokuapp.com':
+    case 'throwback-theater.herokuapp.com':
         APIURL = 'https://throwback-theater-server.herokuapp.com'
 }
 
